19a3194af88e98834556b9f820ff83ec3f3bb2a1
alexander.robinson <alexander.robinson@oit.edu>
2020-03-13 20:29:58 -0700

    Added scripts

---
+++ Scripts/functions.py
@@ -0,0 +1,9 @@
+from math import pi
+def area(radius):
+    """
+    Returns the area of a circle with the given radius.
+    For example:
+    >>> area (5.5)
+    95.033177771091246
+    """
+    return pi * radius ** 2
---
+++ Scripts/ternary.py
@@ -0,0 +1,3 @@
+food = input("What's your favorite food? ")
+reply = 'yuck' if food == 'lamb' else 'yum'
+print(reply)
